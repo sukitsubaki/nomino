82b572963deffd72424775c1e38d9720ca260a39
Suki Tsubaki <58035709+sukitsubaki@users.noreply.github.com>
2025-03-12 16:31:28 +0100

    Create progress.py

---
+++ src/progress.py
@@ -0,0 +1,8 @@
+def show_progress(iteration, total, prefix='', suffix='', length=50, fill='█'):
+    percent = ("{0:.1f}").format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    bar = fill * filled_length + '-' * (length - filled_length)
+    sys.stdout.write(f'\r{prefix} |{bar}| {percent}% {suffix}')
+    sys.stdout.flush()
+    if iteration == total:
+        print()
